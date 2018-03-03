e18621405cb907d190a3bddd2e0a757b7e79c060
gbakkk5951 <526406038@qq.com>
2018-03-03 21:58:14 +0800

    写

--- 草木灰公社模拟赛Ⅰ/边与边权/eaw.cpp
+++ 草木灰公社模拟赛Ⅰ/边与边权/eaw.cpp
@@ -21,12 +21,41 @@ template <typename Type>
 struct Game {
 	int n;
 	vector<int> v;
+	int ans;
+	void init() {
+		read();
+		getAns();
+	}
 	void read() {
 		OI::read(n);
 		v.resize(n);
 		for (int i = 0; i < n; i++) {
 			OI::read(v[i]);
 		}
+	}
+	int zero, rz, lz;
+	void getAns() {
+		for (int i = 0; i < n; i++) {
+			if (v[i] == 0) {
+				rz = i;
+				zero = 1;
+				break;
+			}
+		}
+		if (zero) {
+			for (i = n - 1; i >= 0; i--) {
+				if (v[i] == 0) {
+					lz = i;
+					break;
+				}
+			}
+		}
+		if (zero) {
+			win = ((n - 1 - lz) & 1) || (rz & 1);
+			return;
+		}
+		
+		
 	}
 }game[50005];
 
