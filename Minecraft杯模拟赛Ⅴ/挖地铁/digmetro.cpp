0d50d6fba1fc8974b8d819cb7614174170e90cae
gbakkk5951 <526406038@qq.com>
2018-04-30 22:51:13 +0800

    磨

--- Minecraft杯模拟赛Ⅴ/挖地铁/digmetro.cpp
+++ Minecraft杯模拟赛Ⅴ/挖地铁/digmetro.cpp
@@ -6,20 +6,14 @@ int main() {}
 #include <cctype>
 #include <algorithm>
 #include <functional>
+#include <queue>
 namespace OI {
+struct dp;
+typedef priority_queue<dp, vector<dp>, greater<dp> > heap;
 const int MXN = 500050;
 const int DST = 0, NXT = 1, VAL = 2;
-int head[MXN], edge[MXN << 1][3];
-int eidx;
-void add(int a, int b, int c) {
-	eidx++;
-	edge[eidx][DST] = b;
-	edge[eidx][VAL] = c;
-	edge[eidx][NXT] = head[a];
-	head[a] = eidx;
-}
-int n, k;
-int beg[MXN];
+const int INF = 0x3f3f3f3f;
+const int LEAF = 0, BEG = 1;
 struct dp {
 	int val, id;
 	bool operator < (const dp &b) const {
@@ -37,11 +31,16 @@ struct dp {
 	inline dp operator + (int add) const {
 		return (dp) {val + add, id};
 	}
-	inline dp friend operator + (int add, const dp &b) const {
-		return (dp) {val + add, id};
+	inline dp friend operator + (int add, const dp &b) {
+		return (dp) {b.val + add, b.id};
+	}
+	inline dp operator [] (int new_id) const {
+		return (dp) {val, new_id};
+	}
+	inline int operator () () const {
+		return id;
 	}
 };
-
 dp mx[MXN][3];
 dp ans;
 void dfs1(int nd, int fa) {
@@ -50,33 +49,46 @@ void dfs1(int nd, int fa) {
 		t = edge[e][DST];
 		if (t != fa) {
 			dfs1(t, nd);
-			mx[nd][2] = dp[t] + edge[e][VAL];
-			mx[nd][2].id = t;
+			mx[nd][2] = (dp[t] + edge[e][VAL])[t];
 			sort(mx[nd], mx[nd] + 3, greater<dp>());
 		}
 	}
 }
 void dfs2(int nd, int fa, const dp &from) {
-	mx[nd][2] = from;
-	mx[nd][2].id = fa;
 	sort(mx[nd], mx[nd] + 3, greater<dp>());
 	int t;
 	for (int e = head[nd]; e; e = edge[e][NXT]) {
 		t = edge[e][DST];
 		if (t != fa) {
-			dfs(t, nd, (mx[nd][0] == t ? mx[nd][1] : mx[nd][0]) + edge[e][VAL]);
+			dfs(t, nd, 
+			((mx[nd][0] == t ? mx[nd][1] : mx[nd][0])
+				[nd] + edge[e][VAL]) );
 		}
 	}
 	if (beg[nd]) {
-		mx[nd][0].id = nd;
-		ans = max(ans, nd);
+		ans = max(ans, mx[nd][0][nd]);
 	}
 }
 
 
+
+
 struct _Main {
+	dp far_leaf[MXN];
+	dp far_beg[MXN];
 	
+	int far_pair[MXN][3];
+	int mark[MXN];
 	
+	void push(int nd) {
+		int t;
+		for (int e = head[nd]; e; e = edge[e][NXT]) {
+			t = edge[e][DST];
+			if (!mark[t]) {
+				
+			}
+		}
+	}
 	_Main() {
 		int a, b, c;
 		read(n); read(k);
@@ -89,7 +101,40 @@ struct _Main {
 		}
 		dfs1(1, 0);
 		dfs2(1, 0);
+		root = ans.id;
+		mark(ans.id, 0);
 		
+	}
+	int n, k;
+	int beg[MXN];
+	
+	heap lh, bh, ph;
+	dp get_top(heap &a) {
+		while (!a.empty() && mark[a.top()()]) {
+			a.pop();
+		}
+		return a.empty() ? (dp) {-INF, 0} : heap.top();
+	}
+	dp get_sec(heap &a) {
+		dp tmp, ret;
+		tmp = a.top();
+		a.pop();
+		while (!a.empty() && mark[a.top()()]) {
+			a.pop();
+		}
+		ret = heap.empty() ? (dp) {-INF, 0} : heap.top();	
+		a.push(tmp); 
+		return ret;
+	}
+	
+	int head[MXN], edge[MXN << 1][3];
+	int eidx;
+	void add(int a, int b, int c) {
+		eidx++;
+		edge[eidx][DST] = b;
+		edge[eidx][VAL] = c;
+		edge[eidx][NXT] = head[a];
+		head[a] = eidx;
 	}
 template <typename Type>
 	void read(Type &a) {
