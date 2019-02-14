233761772bae74f065edc12550dff03c97f9518d
gbakkk5951 <526406038@qq.com>
2019-02-14 15:04:02 +0800

    出题

---
+++ 草木灰公社模拟赛Ⅰ/mod树上mod果/std.cpp
@@ -0,0 +1,58 @@
+using namespace std;
+int main() {}
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <cctype>
+#include <algorithm>
+#include <bitset>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <cmath>
+#include <complex>
+#include <cassert>
+namespace OI {
+struct LS {
+	int inline operator [] (int a) {
+		return a << 1;
+	}
+}ls;
+struct RS {
+	int inline operator [] (int a) {
+		return a << 1 | 1;
+	}
+}rs;
+typedef long long lld;
+typedef double lf;
+typedef unsigned u;
+typedef unsigned char uc;
+typedef unsigned long long llu;
+typedef long double Lf;
+typedef complex<lf> com;
+const lf PI = acos(-1.0);
+const int INF = 0x3f3f3f3f;
+const lld LINF = (lld)INF << 32 | INF;
+const int DST = 0, NXT = 1, VAL = 2, FLOW = 2, CST = 3;
+struct _Main {
+	_Main() {
+		
+	}
+template <typename Type>
+	void read(Type &a) {
+		char t, f = 1;
+		while (!isdigit(t = getchar())) {
+			f = t == '-' ? -1 : f;
+		}
+		a = t - '0';
+		while ( isdigit(t = getchar())) {
+			a *= 10; a += t - '0';
+		}
+		a *= f;
+	}
+}std;
+}
