9b33bbd5209e2e21bd8f4710e9bc7499f1705586
gbakkk5951 <526406038@qq.com>
2023-02-25 12:39:02 +0800

    更新内容

--- 出题套件/spj.cpp
+++ 出题套件/spj.cpp
@@ -3,14 +3,7 @@
 #define WA 1
 using namespace std;
 typedef long long lld;
-const int MaxEdgeNum = 2e5 + 10;
-lld StdAnswer;
-lld UserEdgeNum = -1;
-int n, m, total = 0;
-struct FEdge
-{
-	int u, v, c;
-}Edges[MaxEdgeNum];
+
 
 int main(int argc, char *argv[])
 {
@@ -18,63 +11,7 @@ int main(int argc, char *argv[])
 	FILE *f_out = fopen(argv[3], "r");
 	FILE *f_user = fopen(argv[2], "r");
 
-	fscanf(f_in, "%d%d", &n, &m);
-	for (int i = 1; i <= m; ++i)
-	{
-		fscanf(f_in, "%d%d%d", &Edges[i].u, &Edges[i].v, &Edges[i].c);
-	}
-	
-	fscanf(f_out, "%lld", &StdAnswer);
-	
-	lld TotalDist = 0;
-	try 
-	{
-		if (fscanf(f_user, "%lld", &UserEdgeNum) == EOF || UserEdgeNum < 0 || UserEdgeNum > n * 5)
-		{
-			throw ("Invalid Edge Num");
-		}
-		int CurrentVertex = 1;
-		for (int i = 1; i <= UserEdgeNum; ++i)
-		{
-			lld EdgeId = -1;
-			if (fscanf(f_user, "%lld", &EdgeId) == EOF || EdgeId <= 0 || EdgeId > m)
-			{
-				throw ("Invalid Edge Id");
-			}
-			if (CurrentVertex == Edges[EdgeId].u)
-			{
-				CurrentVertex = Edges[EdgeId].v;
-				TotalDist += Edges[EdgeId].c;				
-			}
-			else if (CurrentVertex == Edges[EdgeId].v)
-			{
-				CurrentVertex = Edges[EdgeId].u;
-				TotalDist += Edges[EdgeId].c;
-			}
-			else
-			{
-				throw "Not a Path";
-			}
-		}
-		if (CurrentVertex != 1)
-		{
-			throw "Not end at 1";
-		}
-	}
-	catch (...)
-	{
-		printf("0\n");
-		return WA;
-	}
-	int Score = 40;
-	if (TotalDist <= StdAnswer * 2)
-	{
-		Score += 60;
-	}
-	else if (TotalDist <= StdAnswer * 5)
-	{
-		Score += (int)ceil(60.0 * (StdAnswer * 5 - TotalDist) / (StdAnswer * 3.0));
-	}	
+
 	fclose(f_in);
 	fclose(f_out);
 	fclose(f_user);
